8423faf8ef9a6c8ff3427b8eb51be53a38326fa2
Mikhail Koloskov <mkoloskov1@gmail.com>
2026-03-12 19:49:43 +0500

    решение

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean helloWorld = bean == bean2;
+        System.out.println(helloWorld);
+
+        Cat beanCat = (Cat)applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat)applicationContext.getBean("cat");
+
+        boolean cat = beanCat == beanCat2;
+        System.out.println(cat);
+
         System.out.println(bean.getMessage());
     }
 }
